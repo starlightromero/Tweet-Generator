aeaeb9bd3e5508a024f87d4089391859953e1b2c
Starlight Romero <starlightromero@gmail.com>
2020-11-12 15:25:52 -0800

    refactored function to have a quotes parameter

--- quote.py
+++ quote.py
@@ -1,18 +1,17 @@
 """Import random."""
 import random
 
-quotes = (
-    "It's just a flesh wound.",
-    "He's not the Messiah. He's a very naughty boy!",
-    "THIS IS AN EX-PARROT!!",
-)
 
-
-def random_quote():
+def random_quote(quotes):
     """Return a random quote from quotes."""
     rand_index = random.randint(0, len(quotes) - 1)
     return quotes[rand_index]
 
 
 if __name__ == "__main__":
-    print(random_quote())
+    quote_list = (
+        "It's just a flesh wound.",
+        "He's not the Messiah. He's a very naughty boy!",
+        "THIS IS AN EX-PARROT!!",
+    )
+    print(random_quote(quote_list))
