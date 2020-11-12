729c4f749f55ca9046626af0022e9eb904080b74
Starlight Romero <starlightromero@gmail.com>
2020-11-12 15:27:08 -0800

    refactored function to have sentence as a parameter

--- rearrange.py
+++ rearrange.py
@@ -3,13 +3,13 @@ import re
 import random
 
 
-def rearrange_words():
-    """Return user entered words in a random order."""
-    user_sentence = input("Enter a sentence to randomize: ")
-    sentence_list = re.sub(r"[^\w\s]", "", user_sentence).lower().split(" ")
+def rearrange_words(sentence):
+    """Return setence in a random order."""
+    sentence_list = re.sub(r"[^\w\s]", "", sentence).lower().split(" ")
     random.shuffle(sentence_list)
     return " ".join(sentence_list)
 
 
 if __name__ == "__main__":
-    print(rearrange_words())
+    user_sentence = input("Enter a sentence to randomize: ")
+    print(rearrange_words(user_sentence))
